2dba391e5c15705b6171aa5753e48d90cdd938f8
zeduan <zeduan@cisco.com>
2019-06-02 20:30:05 +0800

    Insert sort

--- src/com/cisco/ds2/sort/InsertSort.java
+++ src/com/cisco/ds2/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package com.cisco.ds2.sort;
 
+import java.util.Arrays;
+
 public class InsertSort {
 
     /**
@@ -9,10 +11,8 @@ public class InsertSort {
      */
     public static void insertSortOrder(Integer[] arr) {
         Integer target;
-        int i, j;
-        for (i = 1; i < arr.length; i++) {
+        for (int i = 1, j = i; i < arr.length; j = ++i) {
             target = arr[i];
-            j = i;
             while (j > 0 && target < arr[j - 1]) {
                 arr[j] = arr[j - 1];
                 j--;
@@ -22,45 +22,32 @@ public class InsertSort {
     }
 
     public static void insertSortOrder1(Integer[] a) {
-        int n = a.length;
-        int i, j;
         Integer target;
-        for (i = 1; i < n; i++) {
-            /**
-             * temp为本次循环待插入有序列表中的数
-             */
+        int low, high, mid;
+        for (int i = 1, j = i; i < a.length; j = ++i) {
             target = a[i];
-            int low = 0;
-            int high = i - 1;
-            /**
-             * 寻找temp插入有序列表的正确位置，使用二分查找法
-             */
+            low = 0;
+            high = i - 1;
             while (low <= high) {
-                /**
-                 * 有序数组的中间坐标，此时用于二分查找，减少查找次数
-                 */
-                int mid = (low + high) / 2;
-                /**
-                 * 若有序数组的中间元素大于待排序元素，则有序序列向中间元素之前搜索，否则向后搜索
-                 */
+                mid = (low + high) / 2;
                 if (a[mid] > target) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             }
-
-            for (j = i - 1; j >= low; j--) {
-                /**
-                 * 元素后移，为插入target做准备
-                 */
-                a[j + 1] = a[j];
+            while (j > low) {
+                a[j] = a[j - 1];
+                j--;
             }
-            /**
-             * 插入temp
-             */
             a[low] = target;
         }
 
+    }
+
+    public static void main(String[] args) {
+        Integer[] data = new Integer[]{1, 4, 3, 2, 6, 8, 3, 4, 9, 3, 1, 7};
+        InsertSort.insertSortOrder1(data);
+        Arrays.stream(data).forEach(System.out::print);
     }
 }
