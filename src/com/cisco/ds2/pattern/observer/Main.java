97331b10aac3e0d7a51da063f73f93e99d39498a
zeduan <zeduan@cisco.com>
2019-06-05 12:03:37 +0800

    observer pattern.

--- src/com/cisco/ds2/pattern/observer/Main.java
+++ src/com/cisco/ds2/pattern/observer/Main.java
@@ -2,14 +2,14 @@ package com.cisco.ds2.pattern.observer;
 
 public class Main {
     public static void main(String[] args) {
-        Stock house = new Stock("fhtx", 100f);
+        Stock stock = new Stock("fhtx", 100f);
         People p1 = new People("p1");
         People p2 = new People("p2");
 
-        house.addObserver(p1);
-        house.addObserver(p2);
+        stock.addObserver(p1);
+        stock.addObserver(p2);
 
-        house.setPrice(111f);
-        house.setPrice(112f);
+        stock.setPrice(111f);
+        stock.setPrice(112f);
     }
 }
